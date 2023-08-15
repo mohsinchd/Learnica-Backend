bc29892f59080e197bdcf0409f948a79a9bae75c
mohsinchd <mohsinshoaib735@gmail.com>
2023-08-15 18:31:55 +0500

    modified: Cookies removed and JWT modifieded

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,17 +4,26 @@ import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decodedData._id);
+
+    req.user = await User.findById(decodedData._id).select("-password");
+
+    next();
+  }
 
   if (!token) {
     return next(
       new ErrorHandler("Only Logged In Users can access this resource", 401)
     );
   }
-
-  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-
-  req.user = await User.findById(decodedData._id).select("-password");
-
-  next();
 });
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,17 +1,19 @@
 export const sendToken = (res, user, message, statusCode = 200) => {
   // Get JWT Token
   const token = user.getJWTToken();
-  // Cookie Options
 
-  let options = {
-    httpOnly: true,
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    sameSite: true,
-  };
-
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).json({
     success: true,
     message,
-    user,
+    user: {
+      _id: user._id,
+      name: user.name,
+      avatar: user.avatar,
+      email: user.email,
+      enrolledCourses: user.enrolledCourses,
+      role: user.role,
+      createdAt: user.createdAt,
+      token,
+    },
   });
 };
